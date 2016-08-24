04db39d31d3f0c946fc110b24f33aa6085bd65fe
delgadoguadamuz <delgadoguadamuz@hotmail.com>
2016-08-23 18:52:00 -0600

    objectos principio

---
+++ objectos/objectos/Estudiante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objectos
+{
+    class Estudiante
+    {
+
+        //1. Atributos
+
+        private string nombre;
+        private int grado;
+        private decimal nota;
+
+
+        // 2. Constructores
+
+        public Estudiante()
+
+        {
+
+
+
+        }
+
+
+    }
+}
